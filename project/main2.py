11c75fda74ceeded5505964b41ad62d0538cc556
Yang-bug-star <68415992+Yang-bug-star@users.noreply.github.com>
2022-07-09 23:42:11 +0800

    Delete main2.py

--- project/main2.py
+++
@@ -1,6 +0,0 @@
-from translate_to_mips import Translate_to_MIPS
-
-filename='result/intermediate.txt'
-path='result/result.asm'
-translate_to_MIPS=Translate_to_MIPS(filename,path)
-translate_to_MIPS.translate()
